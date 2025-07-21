310c3bc6d9238e2191707462f150c4cf48488307
sumannaali <samanaabbas1996@gmail.com>
2025-07-21 01:52:49 -0700

    commit

--- exam_portal/exam_portal/settings.py
+++ exam_portal/exam_portal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ttw+-$3ld!wc*(qtuadfy#-r+o-b4b8ejb&f=a9b-vlwz*k5g^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
